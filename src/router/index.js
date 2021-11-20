c31dc982399c788a6feb3e227f06c80bc8484bc1
Brayan Santos <santos.a.brayan97@gmail.com>
2021-11-20 00:07:29 -0500

    change react-router-dom version

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,35 @@
 import React from "react";
 import Navbar from '../components/Navbar';
 import Show from '../pages/Show';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes, Switch } from 'react-router-dom';
 import routes from './routes';
 import axios from 'axios';
 
 const AppRouter = () => {
 
+	const componentDidMount = () => {
+
+		var options = {
+		  method: 'GET',
+		  url: 'https://covid-19-data.p.rapidapi.com/country/code',
+		  params: {code: 'US', format: 'json'},
+		  headers: {
+		    'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
+		    'x-rapidapi-key': ''
+		  }
+		};
+
+		axios.request(options).then(function (response) {
+			console.log(response.data);
+		}).catch(function (error) {
+			console.error(error);
+		});
+	}
+
 	return (
 		<Router>
       <Navbar routes={routes}/>
-			<Routes>
+			<Switch>
 				{routes.map(({ Component, key, path }) => (
 					<Route
 						key={key}
@@ -23,7 +42,7 @@ const AppRouter = () => {
           path={'/:id'}
           render={routerProps => <Show {...routerProps} />}
         ></Route>
-			</Routes>
+			</Switch>
 		</Router>
 	);
 };
